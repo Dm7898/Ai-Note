5eb089a6d3db59c478c916812bd6f1eaff95ebfb
rasool <rasooli7898>
2025-02-17 23:07:19 +0530

    server link

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/note",
+  baseURL: "https://ai-note-bice.vercel.app/api/note",
   headers: { "Content-Type": "application/json" },
 });
 
